cfc7801ce0932522b05ba28c1df8bc121ce4c51d
Jaredp75 <jaredp75@hotmail.com>
2017-08-23 13:53:22 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//dependencies
+const express = require('express');
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+const mustache = require('mustache');
+const mustache-express = require('mustache-express');
+const parseurl = require('parseurl');
+const path = require('path');
+const session = require('express-session');
+const data = require('');
